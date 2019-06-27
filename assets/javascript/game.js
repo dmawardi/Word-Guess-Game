fd7c3757d5908152e85c1584d008cad618ac32be
Dannar Mawardi <dmawardi@gmail.com>
2019-06-27 16:52:37 +1000

    Basic HTML and reset css file prepared

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// ID List:
+// winsText: How many wins
+// currentWord: Current word being guessed
+// guessesRemaining: Remaining guess counter 
+// lettersGuessed: List of letters guessed for current word
+
