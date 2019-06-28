7ddb5c6355c12ea6fc308b2f0b01266028971fb5
Dannar Mawardi <dmawardi@gmail.com>
2019-06-28 21:21:56 +1000

    wordCharSearcher complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,12 @@
 // guessesRemaining: Remaining guess counter 
 // lettersGuessed: List of letters guessed for current word
 
+// Function list
+
+// wordCharSearcher(word, char)
+// Function that searches input word for the input character
+
+
 // Declare variables and assign to document elements
 var winsText = document.getElementById('winsText');
 var currentWord = document.getElementById('currentWord');
@@ -23,9 +29,49 @@ function randomPicker(wordArray) {
 
 // alert("Random word: " + randomPicker(words));
 
+// Game
+// Player touches a key to start the game
+document.onkeyup = function wordGuessGame() {
+    console.log('The Game has begun');
+    
+    // Select Random word from word Array
+    var guessWord = randomPicker(words);
+    
+}
+
+
+
 // below searches through string starting at position index 5
+
 var n = string.indexOf("e", 5);
 
-function checkIfLetterInWord(guessChar) {
-    
+
+function revealFoundLetters(guessChar) {
+
+}
+
+
+
+// Function that searches input word for the input character
+function wordCharSearcher(word, char) {
+    // Declare variables
+    var lastFoundIndex = 0;
+    var listOfFound = [];
+    var foundIndex = 0;
+
+
+    // Iterate through word
+    for (var i = 0; i < word.length; i++) {
+
+        // Search word for character char, starting at lastFound Index
+        foundIndex = word.indexOf(char, lastFoundIndex);
+
+        // Push the found index to the array of found indices
+        listOfFound.push(foundIndex);
+
+        // Assign the found index to the lastFoundIndex variable
+        lastFoundIndex = foundIndex;
+    }
+
+    return listOfFound;
 }
