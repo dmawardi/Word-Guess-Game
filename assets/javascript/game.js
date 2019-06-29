1ad6345ae178b8936b99b42ca6177f4a83425269
Dannar Mawardi <dmawardi@gmail.com>
2019-06-29 16:25:06 +1000

    Moved functions to top of javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,14 +11,22 @@
 
 
 // Declare variables and assign to document elements
+var messageDisplayText = document.getElementById('messageDisplayText');
 var winsText = document.getElementById('winsText');
 var currentWord = document.getElementById('currentWord');
 var guessesRemaining = document.getElementById('guessesRemaining');
 var lettersGuessed = document.getElementById('lettersGuessed');
 
-
+// Initialize word selection array
 var words = ['hello', 'goodbye', 'test', 'another word'];
 
+// Function List
+// Function Definitions
+// wordCharSearcher(word, char)
+// Function that searches input word for the input character
+
+
+// Function randomPicker: Selects random word from array and returns
 function randomPicker(wordArray) {
 
     // Select random word from wordArray using Math package to random pick number between minimum and array length (maximum)
@@ -26,31 +34,6 @@ function randomPicker(wordArray) {
 
     return word;
 }
-
-// alert("Random word: " + randomPicker(words));
-
-// Game
-// Player touches a key to start the game
-document.onkeyup = function wordGuessGame() {
-    console.log('The Game has begun');
-    
-    // Select Random word from word Array
-    var guessWord = randomPicker(words);
-    
-}
-
-
-
-// below searches through string starting at position index 5
-
-var n = string.indexOf("e", 5);
-
-
-function revealFoundLetters(guessChar) {
-
-}
-
-
 
 // Function that searches input word for the input character
 function wordCharSearcher(word, char) {
@@ -75,3 +58,68 @@ function wordCharSearcher(word, char) {
 
     return listOfFound;
 }
+
+// Function that takes guessWord and currently revealed index and forms string to present to user
+function revealFoundLetters(guessword, revealedLetters) {
+
+}
+
+
+
+
+// Game
+// Player touches a key to start the game
+document.onkeyup = function wordGuessGame() {
+    // Display to user that the game has begun
+    messageDisplayText = 'The Game has Begun!';
+    console.log('The Game has begun');
+    
+    // Select random word from word Array
+    var guessWord = randomPicker(words);
+    console.log('The randomword has been selected: ' + guessWord);
+    
+
+    // Initialize Game variables
+    var guessChances = 12;
+    var currentlyRevealedIndex = [];
+
+    // Set guess chances to 0 and display to user the chances and the guess text
+    
+    guessesRemaining.innerHTML = guessChances.toString();
+    messageDisplayText = 'Guess the ' + guessWord.length + ' letter word';
+
+    
+    // While guess chances are available & the currently revealed guess word letters are, read keystrokes as character guesses for the word
+    while (guessChances > 0 && currentlyRevealedIndex.length < guessWord.length) {
+        console.log('Please key in your guess');
+        // When key pressed
+        document.onkeyup = function(event) {
+            console.log('Key pressed : ' + event.key);
+
+            // Search guess word for user pressed letter and output
+            var foundIndexes = wordCharSearcher(guessword, event.key);
+
+            if (foundIndexes.length > 0) {
+                console.log('You have found ' + foundLetters.length + ' letters in the word!');
+
+                // Add code to reveal found letters based off found
+            }
+            else {
+                console.log('Sorry. Try again!');
+
+                // Reduce guess chances and print to document
+                guessChances--;
+                guessesRemaining = guessChances;
+            }
+
+            
+
+        }
+    }
+
+}
+
+
+
+
+
